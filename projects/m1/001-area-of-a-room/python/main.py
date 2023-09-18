2922700e88c72fd713044b7715c973efb78381ce
Wainar <royaltiger090@gmail.com>
2023-09-18 21:28:27 +0200

    m1-solutions-001-area-of-a-room

---
+++ projects/m1/001-area-of-a-room/python/main.py
@@ -0,0 +1,11 @@
+#area_rooms
+
+width_rooms=float(input("Please enter the width of the room(meters): "))
+
+length_rooms=float(input("Please enter the length of the room(meters): "))
+
+area_rooms= width_rooms*length_rooms
+
+#final calculate area of the rooms
+
+print("The area of the rooms is", area_rooms, "square meters")
